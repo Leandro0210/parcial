6ebce92e0224c95fffb173aa712c4688c23e644c
Leandro <leandromirandaroman@gmail.com>
2024-04-25 16:40:53 -0400

    mi ultimo commit

--- codfuente/obj/Debug/net8.0/parcial.AssemblyInfo.cs
+++ codfuente/obj/Debug/net8.0/parcial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("parcial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f67fe2fdc173cc5dd978c1988f2872c25deb12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba5cce170494c3179522684f4caf1170b521e75")]
 [assembly: System.Reflection.AssemblyProductAttribute("parcial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("parcial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
